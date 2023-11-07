36adb706d41fb7c26b5e9325ce5993610943b085
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-07 20:49:07 +0000

    Remove User model as it already exists in Django

--- booking/models.py
+++ booking/models.py
@@ -1,17 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import Group
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import User
 
-class User(AbstractUser):
-    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
-    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=150, unique=True)
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def __str__(self):
-        return self.email
 # Create your models here.
