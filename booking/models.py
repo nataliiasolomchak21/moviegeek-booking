4b98f845007db179505d73cd79ac67b7945f9492
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-07 14:11:12 +0000

    Create a User model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
 
+class User(AbstractUser):
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
 # Create your models here.
