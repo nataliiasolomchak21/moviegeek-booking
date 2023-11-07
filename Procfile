ea91d26efc6d2678c96e0b3a62300791c4d98fce
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-07 12:17:26 +0000

    Add my local host in ALLOWED_HOSTS

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn moviegeek-booking-1c00719de368.wsgi
+web: gunicorn moviegeek-booking.wsgi
--- moviegeek/settings.py
+++ moviegeek/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['moviegeek-booking-1c00719de368.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['moviegeek-booking-1c00719de368.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
