8e1d6f3c2e089cd533f5a3ca4b75fa38fc7ee8d4
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-09 14:02:07 +0000

    Add view for booking page and connect it up via urls

--- booking/views.py
+++ booking/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def booking(request):
+    return render(request, 'booking.html')
--- moviegeek/urls.py
+++ moviegeek/urls.py
@@ -20,4 +20,5 @@ from booking import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('booking/', views.booking, name='booking'),
 ]
