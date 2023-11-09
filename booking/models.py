08b621bc7f6432180431c865dbed52dbd6c0b19b
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-09 14:23:12 +0000

    Create Booking model and a view for it

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Booking(models.Model):
+    title=models.CharField(max_length=255)
+    price=models.IntegerField()
+    booked_seats=models.ManyToManyField('Seat', null=True, blank=True)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.title} (${self.price})"
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Booking
 
 def index(request):
     return render(request, 'index.html')
 
 def booking(request):
-    return render(request, 'booking.html')
+    movies=Movie.objects.all
+    return render(request, 'booking.html', {
+        "movies": movies
+    })
