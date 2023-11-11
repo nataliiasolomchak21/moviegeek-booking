19b2886027e0e6f4c527cbda27c623d160f61269
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-11 21:26:03 +0000

    Modify the header and titles in the admin side

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Movie, Booking
 
+admin.site.site_header = "MovieGeek Booking Admin"
+admin.site.site_title = "MovieGeek Booking Admin Area"
+admin.site.index_title = "Welcome to the MovieGeek Booking admin area"
+
 admin.site.register(Movie)
 admin.site.register(Booking)
 
