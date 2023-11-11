7806bc41581a78bffc7e3e13f58743bbf9f22a23
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-11 18:11:47 +0000

    Add str method to the models

--- booking/models.py
+++ booking/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2, default='12.00')
+    
+    def __str__(self):
+        return self.title
 
 class Booking(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
@@ -11,3 +14,6 @@ class Booking(models.Model):
     date = models.DateField()
     time = models.TimeField()
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.movie.title} - {self.date} {self.time}"
