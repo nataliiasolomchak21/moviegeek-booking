4d29ab712d5e16e4a08867e529186d04c3f0aa49
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-11 17:17:15 +0000

    Modify Movie model

--- booking/models.py
+++ booking/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2, default=12.00)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default='12.00')
 
 class Booking(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
