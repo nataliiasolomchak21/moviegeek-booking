d03514b24126776efecff65644acb009a2d37d41
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-21 21:43:17 +0000

    Add styling for password rules

--- moviegeek/settings.py
+++ moviegeek/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
