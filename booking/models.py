b85a7f4d5a98ff240b57d54520695c9c0465dd4c
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-11-23 22:29:16 +0000

    Add id to Movie model

--- booking/models.py
+++ booking/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=5, decimal_places=2, default='12')
     
