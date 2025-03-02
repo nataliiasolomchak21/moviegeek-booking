d672f89363112131aa69434fb1c2baec3acd42ff
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-03-02 18:14:51 +0000

    Update build_files.sh file

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Install system dependencies
+apt-get update && apt-get install -y libcairo2-dev
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Apply database migrations
+python3 manage.py migrate
+
+# Collect static files
 python3 manage.py collectstatic --noinput
